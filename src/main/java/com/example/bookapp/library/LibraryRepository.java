4ed6317670ac5128f054b1a3a571cba893309f78
sunkyoung <sunkyoung.dev@gmail.com>
2025-10-28 19:27:32 +0900

    레파지토리

---
+++ src/main/java/com/example/bookapp/library/LibraryRepository.java
@@ -0,0 +1,13 @@
+package com.example.bookapp.library;
+
+import org.apache.tomcat.jni.Library;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class LibraryRepository {
+    public List<Library> getLibraryListByCityandProvince(String city, String province) {
+        return null;
+    }
+}
