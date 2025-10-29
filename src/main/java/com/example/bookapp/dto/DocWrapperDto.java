fbfb30d427fe608e195c610c6161bf216de9b7ac
sunkyoung <sunkyoung.dev@gmail.com>
2025-10-29 14:08:49 +0900

    DocWrapperDto 생성

---
+++ src/main/java/com/example/bookapp/dto/DocWrapperDto.java
@@ -0,0 +1,12 @@
+package com.example.bookapp.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@NoArgsConstructor
+@ToString
+public class DocWrapperDto {
+    private BookDto doc;
+}
