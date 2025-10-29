cbfddb5898516f432c1ae1239c8ecc821b7ba53a
sunkyoung <sunkyoung.dev@gmail.com>
2025-10-29 15:26:30 +0900

    LibratyDto

---
+++ src/main/java/com/example/bookapp/dto/LibraryDto.java
@@ -0,0 +1,24 @@
+package com.example.bookapp.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class LibraryDto {
+    private Long id;
+    private String libName;
+    private String address;
+    private String tel;
+    private float lattiude;
+    private float longitude;
+    private String homepage;
+    private  String closed;
+    private String operatingTime;
+
+
+}
