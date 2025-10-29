fcf52317b9bfe72ac173455cb8787b8f30da7353
sunkyoung <sunkyoung.dev@gmail.com>
2025-10-29 14:08:32 +0900

    restTemplate 주입

--- src/main/java/com/example/bookapp/BookappApplication.java
+++ src/main/java/com/example/bookapp/BookappApplication.java
@@ -2,12 +2,19 @@ package com.example.bookapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BookappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookappApplication.class, args);
+    }
+
+    @Bean //이 메소드가 반환하는 객체를 스프링이 관리하는 Bean으로 등록
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
