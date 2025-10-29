f1f09e280f9982bd15305f04bc427e27b18375ab
sunkyoung <sunkyoung.dev@gmail.com>
2025-10-29 15:38:22 +0900

    LibraryServiceImpl LibraryMapper 삽입

--- src/main/java/com/example/bookapp/library/LibraryServiceImpl.java
+++ src/main/java/com/example/bookapp/library/LibraryServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.bookapp.library;
 
+import com.example.bookapp.dto.LibraryDto;
+import com.example.bookapp.mapper.LibraryMapper;
 import org.apache.tomcat.jni.Library;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +11,10 @@ import java.util.List;
 @Service
 public class LibraryServiceImpl {
 
-    private LibraryRepository libraryRepository;
-    public LibraryServiceImpl(LibraryRepository libraryRepository){
-        this.libraryRepository=libraryRepository;
+
+    private LibraryMapper libraryMapper;
+    public LibraryServiceImpl( ){
+
     }
 
     /**
@@ -25,10 +28,10 @@ public class LibraryServiceImpl {
      */
     public List<?> getLibraryBylocation(double mylantitude, double mylongitude, String city, String province) {
         // 해당 시도의 도서관 리스트
-        List<Library> libraryList =libraryRepository.getLibraryListByCityandProvince(city,province);
-        List<Library> libraryListIn500 = new ArrayList<>();
+        List<LibraryDto> libraryList =libraryMapper.findLibraryBycityandprovince(city,province);
+        List<LibraryDto> libraryListIn500 = new ArrayList<>();
         // 리스트를 돌려가면서 500m 안인지 계산하는  공식 메서드
-        for(Library library:libraryList){
+        for(LibraryDto library:libraryList){
             boolean isWithinRadius=GeoDistanceCalculator.isWithinRadius(mylantitude,mylongitude,1.11,1.12,500.0);
             if(isWithinRadius){
                 libraryListIn500.add(library);
