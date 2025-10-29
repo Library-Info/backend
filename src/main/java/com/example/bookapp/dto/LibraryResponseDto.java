3d6611fef255cd53056e28f1663dd05016ea08ef
sunkyoung <sunkyoung.dev@gmail.com>
2025-10-29 14:09:17 +0900

    LibraryResponseDto 작성

---
+++ src/main/java/com/example/bookapp/dto/LibraryResponseDto.java
@@ -0,0 +1,14 @@
+package com.example.bookapp.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@ToString
+public class LibraryResponseDto {
+    private List<BookDto> docs;
+}
