995c7029acc9828b7da2d5b50d8361b634643228
sunkyoung <sunkyoung.dev@gmail.com>
2025-11-02 00:15:23 +0900

    SearchController.java : searchLibraryListByisbn  작성

--- src/main/java/com/example/bookapp/library/SearchController.java
+++ src/main/java/com/example/bookapp/library/SearchController.java
@@ -1,7 +1,6 @@
 package com.example.bookapp.library;
 
-import com.example.bookapp.dto.BookDto;
-import com.example.bookapp.dto.DocWrapperDto;
+import com.example.bookapp.dto.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,14 +30,18 @@ public class SearchController {
     }
 
     /**
-     * isbn 번호를 토대로 그 도서를 소장하고 있는 도서관을 나열 합니다.
-     * @param isbn
-     * @return
+     * isbn 번호를 토대로 그 도서를 소장하고 있는 을 나열 합니다.
+     * @param searchLibraryByLocationRequestDto
+     * @return List<LibraryAvailabilityDto>
+     *     LibraryAvailabilityDto 안에 도서관 코드, 책 소장여부, 책 대출 가능 여부 들어있음
+     *     가능하면 Y, 불가능하면 N 출력됨.
      * @throws MalformedURLException
      */
-//    @GetMapping("/library")
-//    public ResponseEntity<?>  searchLibraryByLocation(@RequestParam int isbn)  {
-//
-//    }
+    @PostMapping("/isbn/library")
+    public ResponseEntity<?>  searchLibraryByLocation(@RequestBody searchLibraryByLocationRequestDto dto)  {
+        List<LibraryAvailabilityDto> searchLibraryListByisbn =searchService.searchLibraryByIsbn(dto.getIsbn13(),dto.getLibraryCodeList());
+        return ResponseEntity.ok(searchLibraryListByisbn);
+
+    }
 
 }
