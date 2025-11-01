cae8c3a0e1a92898302f77b98645acea8d25c38a
sunkyoung <sunkyoung.dev@gmail.com>
2025-11-02 00:13:38 +0900

    isbnResponseDto.java : isbn번호 토대로 도서관 책 소장, 대출여부 dto

---
+++ src/main/java/com/example/bookapp/dto/isbnResponseDto.java
@@ -0,0 +1,15 @@
+package com.example.bookapp.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@ToString
+public class isbnResponseDto {
+    ResultWrapperDto result;
+
+}
