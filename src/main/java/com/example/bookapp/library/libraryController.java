45390ce65cd79f952a5075a8f90dc344056a5a6f
sunkyoung <sunkyoung.dev@gmail.com>
2025-11-01 23:03:40 +0900

    @CrossOrigin 추가

--- src/main/java/com/example/bookapp/library/libraryController.java
+++ src/main/java/com/example/bookapp/library/libraryController.java
@@ -1,13 +1,10 @@
 package com.example.bookapp.library;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/library")
 public class libraryController {
