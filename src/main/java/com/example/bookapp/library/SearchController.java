6e6035c017f1a9b4515824d876c05f397c3d2228
sunkyoung <sunkyoung.dev@gmail.com>
2025-11-01 23:03:54 +0900

    @CrossOrigin 추가

--- src/main/java/com/example/bookapp/library/SearchController.java
+++ src/main/java/com/example/bookapp/library/SearchController.java
@@ -3,15 +3,12 @@ package com.example.bookapp.library;
 import com.example.bookapp.dto.BookDto;
 import com.example.bookapp.dto.DocWrapperDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.print.Book;
 import java.net.MalformedURLException;
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/search")
 public class SearchController {
@@ -32,5 +29,16 @@ public class SearchController {
         return ResponseEntity.ok(searchBookList);
 
     }
+
+    /**
+     * isbn 번호를 토대로 그 도서를 소장하고 있는 도서관을 나열 합니다.
+     * @param isbn
+     * @return
+     * @throws MalformedURLException
+     */
+//    @GetMapping("/library")
+//    public ResponseEntity<?>  searchLibraryByLocation(@RequestParam int isbn)  {
+//
+//    }
 
 }
