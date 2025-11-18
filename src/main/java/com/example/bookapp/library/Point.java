0cda6df5c22f40cb5eb31fb9631d568c0005e6d0
sunkyoung <sunkyoung.dev@gmail.com>
2025-11-18 20:59:44 +0900

    Point.java

---
+++ src/main/java/com/example/bookapp/library/Point.java
@@ -0,0 +1,16 @@
+package com.example.bookapp.library;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+// 공통 좌표(x, y)를 위한 클래스
+@Getter
+@Setter
+@AllArgsConstructor
+public class Point {
+    private Double x;
+    private Double y;
+
+
+}
