2509d684cb1f169f0ea026c30f55dea9f8dc1ca4
sunkyoung <sunkyoung.dev@gmail.com>
2025-11-18 20:55:10 +0900

    GenderDto

---
+++ src/main/java/com/example/bookapp/dto/GenderDto.java
@@ -0,0 +1,17 @@
+package com.example.bookapp.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class GenderDto {
+
+    private int code;
+
+
+}
