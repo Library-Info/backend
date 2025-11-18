6205a06c69e0e51094f0153cbce1f1870d3ca320
sunkyoung <sunkyoung.dev@gmail.com>
2025-11-18 21:00:44 +0900

    getRecommedBook 추가

--- src/main/java/com/example/bookapp/library/LibraryServiceImpl.java
+++ src/main/java/com/example/bookapp/library/LibraryServiceImpl.java
@@ -1,21 +1,38 @@
 package com.example.bookapp.library;
 
-import com.example.bookapp.dto.LibraryDto;
+import com.example.bookapp.dto.*;
+import com.example.bookapp.library.controller.GeoDistanceCalculator;
 import com.example.bookapp.mapper.LibraryMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class LibraryServiceImpl {
 
+    @Autowired
+    private RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+
     @Value("${my.SearchRadiusMeter}")
     private  double  SEARCH_RADIUS_METER;
 
+    @Value("${my.authkey")
+    private String authkey;
+
     private final LibraryMapper libraryMapper;
-    public LibraryServiceImpl(LibraryMapper libraryMapper ) {
+    public LibraryServiceImpl(ObjectMapper objectMapper, LibraryMapper libraryMapper ) {
+        this.objectMapper = objectMapper;
         this.libraryMapper = libraryMapper;
 
     }
@@ -36,7 +53,7 @@ public class LibraryServiceImpl {
         List<LibraryDto> libraryListIn500 = new ArrayList<>();
         // 리스트를 돌려가면서 500m 안인지 계산하는  공식 메서드
         for(LibraryDto library:libraryList){
-            boolean isWithinRadius=GeoDistanceCalculator.isWithinRadius(mylantitude,mylongitude,library.getLatitude(),library.getLongitude(),SEARCH_RADIUS_METER);
+            boolean isWithinRadius= GeoDistanceCalculator.isWithinRadius(mylantitude,mylongitude,library.getLatitude(),library.getLongitude(),SEARCH_RADIUS_METER);
             if(isWithinRadius){
                 libraryListIn500.add(library);
             }
@@ -51,4 +68,41 @@ public class LibraryServiceImpl {
     }
 
 
+    public List<?> getRecommendBookByFilter(int age, String gender) {
+        int ageCode=libraryMapper.findAgeCode(age);
+        int genderCode= libraryMapper.findGenderCode(gender);
+        int regionCode=0;
+
+        URI uri;
+        uri= UriComponentsBuilder
+                .fromUriString("http://data4library.kr")
+                .path("/api/loanItemSrchByLib")
+                .queryParam("authKey",authkey)
+                .queryParam("region",regionCode)
+                .queryParam("gender",genderCode)
+                .queryParam("age",ageCode)
+                .queryParam("pageNO",1)
+                .queryParam("pageSize",5)
+                .queryParam("format","json")
+                .encode()
+                .build()
+                .toUri();
+        try{
+            String jsonString = restTemplate.getForObject(uri, String.class);
+            JsonNode rootNode = objectMapper.readTree(jsonString);
+            JsonNode responseNode = rootNode.path("response");
+            RecommededBookResponseDto libraryResponseDto = objectMapper.treeToValue(responseNode, RecommededBookResponseDto.class);
+
+            return libraryResponseDto.getDocs().stream() //.stream 으로 리스트를 스트림으로 변환
+                    .map(RedDocWrapperDto::getDoc)
+                    .collect(Collectors.toList());
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
+
+
+    }
 }
