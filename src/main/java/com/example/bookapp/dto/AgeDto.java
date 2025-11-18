4e10681bc583918f0a3a0c679fe6fe4922eb55b5
sunkyoung <sunkyoung.dev@gmail.com>
2025-11-18 20:54:54 +0900

    AgeDto

---
+++ src/main/java/com/example/bookapp/dto/AgeDto.java
@@ -0,0 +1,19 @@
+package com.example.bookapp.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class AgeDto {
+
+    private int code;
+    private String details;
+
+
+
+}
