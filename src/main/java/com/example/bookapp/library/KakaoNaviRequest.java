91b7c2ab8d200c7b1b90e66f86c74223d49ce5b7
sunkyoung <sunkyoung.dev@gmail.com>
2025-11-18 20:59:23 +0900

    KakaoNaviRequest.java

---
+++ src/main/java/com/example/bookapp/library/KakaoNaviRequest.java
@@ -0,0 +1,18 @@
+package com.example.bookapp.library;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Getter
+@Service
+@AllArgsConstructor
+// API 요청 전체를 감싸는 클래스
+public class KakaoNaviRequest {
+    private Point origin;
+    private List<Destination> destinations;
+    private int radius;
+
+
+}
