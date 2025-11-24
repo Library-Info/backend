0ee551b6c35d6d3880f6434ddd43fd73511e6d5b
sunkyoung <sunkyoung.dev@gmail.com>
2025-11-24 20:44:26 +0900

    @NoArgsConstructor 추가

--- src/main/java/com/example/bookapp/library/Point.java
+++ src/main/java/com/example/bookapp/library/Point.java
@@ -2,12 +2,14 @@ package com.example.bookapp.library;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 // 공통 좌표(x, y)를 위한 클래스
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Point {
     private Double x;
     private Double y;
