e6cb4e0e4b8d1bc6c8ca8753e584bf098b804d83
sunkyoung <sunkyoung.dev@gmail.com>
2025-11-24 20:44:29 +0900

    @NoArgsConstructor 추가

--- src/main/java/com/example/bookapp/library/KakaoNaviRequest.java
+++ src/main/java/com/example/bookapp/library/KakaoNaviRequest.java
@@ -2,12 +2,14 @@ package com.example.bookapp.library;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Getter
 @Service
 @AllArgsConstructor
+@NoArgsConstructor
 // API 요청 전체를 감싸는 클래스
 public class KakaoNaviRequest {
     private Point origin;
