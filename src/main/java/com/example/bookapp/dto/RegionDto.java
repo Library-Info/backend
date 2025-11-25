1a3a0563a27f9ba52e4d4ab5727c4bd08b718968
sunkyoung <sunkyoung.dev@gmail.com>
2025-11-25 17:37:05 +0900

    RegionDto 추가

---
+++ src/main/java/com/example/bookapp/dto/RegionDto.java
@@ -0,0 +1,18 @@
+package com.example.bookapp.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class RegionDto {
+
+    private int code;
+
+
+
+}
