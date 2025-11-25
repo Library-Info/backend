cca4c5c18b1ab238f77f0518034d774e088162d8
sunkyoung <sunkyoung.dev@gmail.com>
2025-11-25 19:51:47 +0900

    key 세팅되게 수정

--- src/main/java/com/example/bookapp/library/Destination.java
+++ src/main/java/com/example/bookapp/library/Destination.java
@@ -1,6 +1,15 @@
 package com.example.bookapp.library;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 // key 필드가 추가된 목적지 클래스
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Destination extends Point {
     private String key;
 
